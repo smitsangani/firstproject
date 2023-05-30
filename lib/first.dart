21696fe1041d048d1ab2accd556267bd225a351a
harmit <flutterdeveloper888@gmail.com>
2023-05-30 19:30:06 +0530

    final 2

---
+++ lib/first.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class first extends StatefulWidget {
+  const first({Key? key}) : super(key: key);
+
+  @override
+  State<first> createState() => _firstState();
+}
+
+class _firstState extends State<first> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
